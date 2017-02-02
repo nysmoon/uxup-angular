4d7d573a2ac73c9346e81964b130a352bf67a2c2
Natasha Sukhinina <natashas@openkey.io>
2017-02-01 19:17:33 -0600

    moved from rails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+declare var Waypoint: any;
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  
+	ngAfterViewInit() {
+				
+		$('.scrollimation').waypoint({
+		  handler: function() {
+		    $(this.element).addClass('in');
+		  },
+		  offset:'95%'
+		});
+	}
+	
+	
 }
